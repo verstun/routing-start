8f584f0d7aa30f16f9cdc9a1745c600c8c821299
verstun <svetazavolokina81@gmail.com>
2017-05-12 23:20:44 -0400

    protection of child of servers only

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -4,14 +4,15 @@
     import {CanActivate,
             ActivatedRouteSnapshot,
             RouterStateSnapshot,
-            Router} from '@angular/router';
+            Router,
+            CanActivateChild} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
     constructor(private authService: AuthService, private router: Router){}
     canActivate(route: ActivatedRouteSnapshot,
                 state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
@@ -26,6 +27,12 @@ export class AuthGuard implements CanActivate {
                 }
             }
         );
+
+    }
+
+    canActivateChild(route: ActivatedRouteSnapshot,
+                     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+        return this.canActivate(route, state);
 
     }
 }
